a9c10554fb197d228cfd2a18166b06c428e06814
Sanfire <71272123+ebaerwald@users.noreply.github.com>
2023-05-03 14:03:44 +0200

    frontend

---
+++ ranch-frontend/pages/components/chat.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classNames from 'classnames'
+import styles from '../../styles/main.module.scss';
+import Image from 'next/image'
+
+export default function Chat()
+{
+    return (
+        <div className={classNames(styles.chatbox)}>
+            
+        </div>
+    )
+}
--- ranch-frontend/pages/index.tsx
+++ ranch-frontend/pages/index.tsx
@@ -1,15 +1,34 @@
-import type { NextPage } from 'next'
-import React from 'react'
-import classNames from 'classnames'
-// import styles from '../styles/landingPage.module.scss';
-
+import type { NextPage } from 'next';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import styles from '../styles/main.module.scss';
+import Logo from './components/logo';
+import Chat from './components/chat';
+import Sidebar from './components/sidebar';
 
 
 const landingPage: NextPage = () => {
+
+    const [pageStyle, setPageStyle] = useState(classNames(styles.landingPage));
+    const [content, setContent] = useState<JSX.Element>();
+
     return (
         <div>
-            <div></div>
-            <button>GO</button>
+            <Sidebar/>
+            <div className={pageStyle}>
+                <div>
+                    <Logo/>
+                    <div className={classNames(styles.goButtonContainer)}>
+                    <button onClick={() =>{
+                        setPageStyle(classNames(styles.loginPage));
+                        setContent(<Chat/>);
+                        (document.getElementById('goButton') as HTMLElement).style.display = 'none';
+                    }} 
+                    className={classNames(styles.goButton)} id='goButton'>Go</button>
+                </div>
+            </div>
+                {content}
+            </div>
         </div>
     )
 }
