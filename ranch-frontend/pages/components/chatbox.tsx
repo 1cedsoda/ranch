7407f032bbea5d352c2093d5099e41d3ff28aa85
Sanfire <71272123+ebaerwald@users.noreply.github.com>
2023-05-08 13:34:27 +0200

    frontend

--- ranch-frontend/pages/components/chatbox.tsx
+++ ranch-frontend/pages/components/chatbox.tsx
@@ -3,13 +3,16 @@ import classNames from 'classnames'
 import styles from '../../styles/main.module.scss';
 import Logo from './logo';
 
-function sendMessage(setMessages : Dispatch<SetStateAction<JSX.Element>>, messages : JSX.Element, currentLetter : string, letter : MutableRefObject<string>)
+var letter = '';
+
+function sendMessage(setMessages : Dispatch<SetStateAction<JSX.Element>>, messages : JSX.Element)
 {
     const message = (document.getElementById('messageArea') as HTMLTextAreaElement).value;
     if (message == '') return;
     const words = message.split(' ');
     if (words.length == 1 && (document.getElementById('sidebar') as HTMLElement).style.transform != 'translateX(0)')
     {
+        letter = message.charAt(0).toUpperCase();
         showSidebar();
         setMessages(
             <>
@@ -17,7 +20,7 @@ function sendMessage(setMessages : Dispatch<SetStateAction<JSX.Element>>, messag
             <div className={classNames(styles.messageDiv)}>
                 <svg fill="#000000" width="2.5rem" height="2.5rem" xmlns="http://www.w3.org/2000/svg">
                     <circle cx="20" cy="20" r="1.25rem"></circle>
-                    <text x="20" y="25" fill="white" text-anchor="middle">{currentLetter}</text>
+                    <text x="15" y="25" fill="white">{letter}</text>
                 </svg>
                 <span className={classNames(styles.spanMessage)}>{message}</span>
             </div>
@@ -36,7 +39,7 @@ function sendMessage(setMessages : Dispatch<SetStateAction<JSX.Element>>, messag
         <div className={classNames(styles.messageDiv)}>
             <svg fill="#000000" width="2.5rem" height="2.5rem" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="20" cy="20" r="1.25rem"></circle>
-                <text x="20" y="25" fill="white" text-anchor="middle">{currentLetter}</text>
+                <text x="15" y="25" fill="white">{letter}</text>
             </svg>
             <span className={classNames(styles.spanMessage)}>{message}</span>
         </div>
@@ -57,7 +60,6 @@ function showSidebar()
 
 export default function Chatbox()
 {
-    const letter = useRef<string>('');
     
     const [messages, setMessages] = useState(
         <div className={classNames(styles.messageDiv)}>
@@ -68,19 +70,19 @@ export default function Chatbox()
     document.addEventListener('keydown', function(event) {
         if (event.key === 'Enter' && document.activeElement?.id == 'messageArea')
         {
-            sendMessage(setMessages, messages, letter.current, letter);
+            sendMessage(setMessages, messages);
         }
     });
 
     return (
-        <div>
+        <div className={classNames(styles.chatboxComponent)}>
             <Logo className={classNames(styles.logoBar)} h1Classname={classNames(styles.h1MainPage)} logoClassname={classNames(styles.logoMainPage)}/>
             <div className={classNames(styles.chatbox)} id="chatbox">
                 {messages}
             </div>
             <div className={classNames(styles.messagebox)}>
                 <textarea className={classNames(styles.textinput)} rows={1} placeholder='Send a message.' id="messageArea"/>
-                <img src="/send.svg" alt="Send Logo" className={classNames(styles.messageboxIcon)} onClick={() => sendMessage(setMessages, messages, letter.current, letter)}/>
+                <img src="/send.svg" alt="Send Logo" className={classNames(styles.messageboxIcon)} onClick={() => sendMessage(setMessages, messages)}/>
             </div>
         </div>
     )
