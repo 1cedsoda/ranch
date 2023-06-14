e1f5793f3f0b3d2bf0e10e48344579ff6ed47469
Yurii Fedorenko <yfedorenko@vater-gruppe.de>
2023-06-14 09:45:22 +0200

    new structure implemented

--- ranch-frontend/pages/index.tsx
+++ ranch-frontend/pages/index.tsx
@@ -7,6 +7,7 @@ import Chatbox from '../components/chatbox';
 import Sidebar from '../components/sidebar';
 import { useRouter } from 'next/router';
 import { selectAuthStore } from '../stores/auth';
+import Button from '../components/button';
 
 const landingPage: NextPage = () => {
     
@@ -20,10 +21,28 @@ const landingPage: NextPage = () => {
     }, [authState]);
 
     return (
-        <div className={classNames(styles.mainPage)}>
-            <Sidebar/>
-            <Chatbox />
-        </div>
+        <div>
+        <video className='test' src="test.mov" autoPlay loop muted />
+          <style>
+            @import
+            url('https://fonts.googleapis.com/css2?family=Rubik+Wet+Paint&display=swap');
+          </style>
+          <div className={classNames(styles.main)}>
+            <div className={classNames(styles.logoContainer)}>
+              <div className={classNames(styles.logo)}>
+                <img src="/ranchLogo.jpg" alt="Ranch Logo" />
+              </div>
+              <div className={classNames(styles.title)}>
+                <p>
+                  <h1>Ranch</h1>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={classNames(styles.buttonContainer)}>
+            <Button />
+          </div>
+      </div>
     )
 }
 
